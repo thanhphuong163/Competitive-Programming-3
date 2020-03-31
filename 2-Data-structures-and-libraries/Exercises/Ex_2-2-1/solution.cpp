8eeb4db8fadb0ffc806111453db2e16ae6a26fd9
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-03-31 16:41:06 +0700

    solved Ex221 q5

--- 2-Data-structures-and-libraries/Exercises/Ex_2-2-1/solution.cpp
+++ 2-Data-structures-and-libraries/Exercises/Ex_2-2-1/solution.cpp
@@ -77,6 +77,39 @@ void solve_3(vector<int> vi, int v) {
     cout << endl;
 }
 
+void solve_4(vector<int> vi, int a, int b) {
+    vector<int> result;
+    for (const auto &i: vi) {
+        if (i >= a && i <= b) {
+            result.push_back(i);
+        }
+    }
+    sort(result.begin(), result.end());
+    for (const int &i: result) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int solve_5(vector<int> vi) {
+    int head = 0;
+    int tail = 1;
+    int longest_length = 0;
+    while (tail < vi.size()) {
+        if (vi[tail] < vi[tail + 1]) {
+            tail++;
+            if (tail - head + 1 > longest_length) {
+                longest_length = tail - head + 1;
+            }
+        }
+        else {
+            tail++;
+            head = tail;
+        }
+    }
+    return longest_length;
+}
+
 int main(int argc, char const *argv[]) {
     int n_testcase;
     scanf("%d\n", &n_testcase);
@@ -97,6 +130,8 @@ int main(int argc, char const *argv[]) {
         }
         solve_2(vi, 5);
         solve_3(vi, 5);
+        solve_4(vi, 2, 7);
+        cout << "The length of the longest increasing contiguous sub-array is " << solve_5(vi) << endl;
         // for (int i=0; i < vi.size(); i++) {
         //     cout << vi[i] << " ";
         // }
