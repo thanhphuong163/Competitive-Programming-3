b8fe35526637feb32b19c1841ea9f4b9b13060aa
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-03-31 16:53:30 +0700

    initialize

---
+++ 2-Data-structures-and-libraries/Exercises/Ex_2-2-2/solution.cpp
@@ -0,0 +1,18 @@
+// Author: Nguyen Thanh Phuong
+// Email: thanhphuong.its@gmail.com
+// Problem: 
+// Submit: 
+
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int n_testcase;
+    scanf("%d", &n_testcase);
+    while (n_testcase > 0) {
+        /* your code */
+        n_testcase--;
+    }
+    return 0;
+}
