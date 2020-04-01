9845845178613c92a1384e5089cd56d7b9b87fbf
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-01 12:57:03 +0700

    Initilize

---
+++ 2-Data-structures-and-libraries/1D-array-manipulation/Permutation Arrays/perm_arr.cpp
@@ -0,0 +1,18 @@
+// Author: Nguyen Thanh Phuong
+// Email: thanhphuong.its@gmail.com
+// Problem: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=623&page=show_problem&problem=423
+// Submit: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=submit_problem&problemid=423&category=0
+
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int n_testcase;
+    scanf("%d", &n_testcase);
+    while (n_testcase > 0) {
+        /* your code */
+        n_testcase--;
+    }
+    return 0;
+}
---
+++ 2-Data-structures-and-libraries/1D-array-manipulation/Permutation Arrays/run.sh
@@ -0,0 +1,4 @@
+#! /bin/zsh
+
+g++ -o perm_arr.o perm_arr.cpp
+./perm_arr.o < input.txt
