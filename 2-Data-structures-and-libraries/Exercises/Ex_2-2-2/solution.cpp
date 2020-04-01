98c8ecc55724f1056fa5c2a89ecef5e31e5cd1f1
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-01 21:11:13 +0700

    solved Ex222 q1

--- 2-Data-structures-and-libraries/Exercises/Ex_2-2-2/solution.cpp
+++ 2-Data-structures-and-libraries/Exercises/Ex_2-2-2/solution.cpp
@@ -4,15 +4,29 @@
 // Submit: 
 
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    int n_testcase;
-    scanf("%d", &n_testcase);
-    while (n_testcase > 0) {
-        /* your code */
-        n_testcase--;
+int solve_1(int number, int divider) {
+    // Find # shift steps
+    int n = 0;
+    while (divider != 1) {
+        n++;
+        divider = divider >> 1;
     }
+    // Find the remainder
+    int temp = (number >> n) << n;
+    return number - temp;
+}
+
+int main(int argc, char const *argv[]) {
+    // int n_testcase;
+    // scanf("%d", &n_testcase);
+    // while (n_testcase > 0) {
+    //     /* your code */
+    //     n_testcase--;
+    // }
+    cout << solve_1(19, 8) << endl;
     return 0;
 }
