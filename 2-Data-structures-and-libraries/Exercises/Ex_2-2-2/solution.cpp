251f5eac876e710b9968c9848f6b66e08431dc4e
Nguyen Thanh Phuong <thanhphuong.its@gmail.com>
2020-04-01 21:39:52 +0700

    solved Ex222 q3

--- 2-Data-structures-and-libraries/Exercises/Ex_2-2-2/solution.cpp
+++ 2-Data-structures-and-libraries/Exercises/Ex_2-2-2/solution.cpp
@@ -20,6 +20,30 @@ int solve_1(int number, int divider) {
     return number - temp;
 }
 
+bool solve_2(int num) {
+    int temp = num;
+    int n = 0;
+    while (temp != 1)
+    {
+        n++;
+        temp = temp >> 1;
+    }
+
+    return num == (num >> n) << n;
+}
+
+int solve_3(int S) {
+    int a = 1 << 31;
+    int tmp = S;
+    int n = 31;
+    while(tmp != a) {
+        tmp = tmp << 1;
+        n--;
+    }
+    S ^= (1 << n);
+    return S;
+}
+
 int main(int argc, char const *argv[]) {
     // int n_testcase;
     // scanf("%d", &n_testcase);
@@ -28,5 +52,8 @@ int main(int argc, char const *argv[]) {
     //     n_testcase--;
     // }
     cout << solve_1(19, 8) << endl;
+    int n = 1024;
+    cout << "Is " << n << " a power of 2? " << solve_2(n) << endl;
+    cout << solve_3(18) << endl;
     return 0;
 }
